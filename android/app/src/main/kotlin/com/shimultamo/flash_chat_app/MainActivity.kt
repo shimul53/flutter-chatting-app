4a09b370968171e15398d316d1ed7d08ab920dc8
Shimul Mondol Tamo <shimultamo@gmail.com>
2021-07-08 21:01:20 +0600

    first commit

---
+++ android/app/src/main/kotlin/com/shimultamo/flash_chat_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.shimultamo.flash_chat_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/registration_screen.dart
@@ -0,0 +1,60 @@
+import 'package:flash_chat_app/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:flash_chat_app/components/rounded_button.dart';
+
+class RegistrationScreen extends StatefulWidget {
+  static const String id = 'registration_screen';
+  @override
+  _RegistrationScreenState createState() => _RegistrationScreenState();
+}
+
+class _RegistrationScreenState extends State<RegistrationScreen> {
+  static const String id = 'welcome_screen';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black12,
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Hero(
+              tag: 'logo',
+              child: Container(
+                height: 200.0,
+                child: Image.asset('images/logo.png'),
+              ),
+            ),
+            SizedBox(
+              height: 48.0,
+            ),
+            TextField(
+              onChanged: (value) {
+                //Do something with the user input.
+              },
+              decoration:
+                  kTextFieldDecoration.copyWith(hintText: 'Enter Your Email'),
+            ),
+            SizedBox(
+              height: 8.0,
+            ),
+            TextField(
+              onChanged: (value) {
+                //Do something with the user input.
+              },
+              decoration: kTextFieldDecoration.copyWith(
+                  hintText: 'Enter your password'),
+            ),
+            SizedBox(
+              height: 24.0,
+            ),
+            RoundedButton(
+                title: 'Register', colour: Colors.blueAccent, onPressed: () {})
+          ],
+        ),
+      ),
+    );
+  }
+}
